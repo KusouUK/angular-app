04cec3499f080cc4036c896254a5c58f6b69c8d1
KusouUK <orion26br@gmail.com>
2024-10-09 16:42:03 -0300

    user component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { DUMMY_USERS } from '../dummy-users';
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -8,5 +10,5 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-
+    selectedUser = DUMMY_USERS[Math.floor(Math.random() * DUMMY_USERS.length)];
 }
