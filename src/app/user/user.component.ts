4a581b091e0d44a87eb05451b08615fdda6a1a9b
KusouUK <orion26br@gmail.com>
2024-10-10 17:53:16 -0300

    user button click event and some other stuff i guess

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -7,8 +7,21 @@ import { DUMMY_USERS } from '../dummy-users';
   standalone: true,
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
-    selectedUser = DUMMY_USERS[Math.floor(Math.random() * DUMMY_USERS.length)];
+  selectedUser = DUMMY_USERS[Math.floor(Math.random() * DUMMY_USERS.length)];
+
+  get imagePath() {
+    return '/users/' + this.selectedUser.avatar;
+  }
+
+  onSelectUser() {
+    const filteredDummyUsers = DUMMY_USERS.filter(
+      (u) => u.id != this.selectedUser.id
+    );
+
+    this.selectedUser =
+      filteredDummyUsers[Math.floor(Math.random() * filteredDummyUsers.length)];
+  }
 }
