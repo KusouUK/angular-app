a405c332a40b04edcfc0ea1de44584a7837e3f05
KusouUK <orion26br@gmail.com>
2024-10-15 10:32:23 -0300

    modified some stuff, added ngFor to render the users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgFor } from '@angular/common';
 
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
@@ -8,19 +9,19 @@ import { TasksComponent } from "./tasks/tasks.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
+  imports: [RouterOutlet, NgFor, HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
-  selectedUserId: string = 'u1';
+  selectedUserId: string = '';
 
   get selectedUser() {
     return this.users.find(u => u.id == this.selectedUserId);
   }
 
   onSelectUser(id: string) {
-    this.selectedUserId = this.users.find(u => u.id == id)!.id;
+    this.selectedUserId = id;
   }
 }
